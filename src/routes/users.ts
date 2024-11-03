a693f10b7ff533dc171a2fc3716062315e20e4e5
Haedarah Ali <haedarah.ali.2001@gmail.com>
2024-11-03 15:31:44 +0400

    added_put_api

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,9 +1,10 @@
 import { Router, Request, Response } from 'express';
-import { getUserPoints, getTotalPoints } from '../controllers/userController';
+import { getUserPoints, getTotalPoints, updateUserPoints } from '../controllers/userController';
 
 const router = Router();
 
 router.get('/points_details', getUserPoints);
 router.get('/total_points', getTotalPoints);
+router.put('/put', updateUserPoints);
 
 export default router;
