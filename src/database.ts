650427ba97c526d858bf5af648b9de3fecfeacca
Haedarah Ali <haedarah.ali.2001@gmail.com>
2024-11-04 14:28:42 +0400

    Update database.ts

--- src/database.ts
+++ src/database.ts
@@ -6,10 +6,11 @@ dotenv.config();
 //connects the mongoDB database
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI || '');
-        console.log('MongoDB connected');
+        //await mongoose.connect(process.env.MONGODB_URI || ''); //For local Database
+        await mongoose.connect(process.env.MONGO_URI || ''); //For Atlas Database
+        console.log('Atlas MongoDB connected');
     } catch (error) {
-        console.error('MongoDB connection error:', error);
+        console.error('Atlas MongoDB connection error:', error);
         process.exit(1);
     }
 };
