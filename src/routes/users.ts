f1710440262829ce98d877806108896855127417
Haedarah Ali <haedarah.ali.2001@gmail.com>
2024-11-11 14:17:14 +0400

    first commit

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,10 +1,11 @@
 import { Router, Request, Response } from 'express';
-import { getUserPoints, getTotalPoints, updateUserPoints } from '../controllers/userController';
+import { getUserPoints, getTotalPoints, getAllPoints, updateUserPoints } from '../controllers/userController';
 
 const router = Router();
 
 router.get('/points_details', getUserPoints);
 router.get('/total_points', getTotalPoints);
+router.get('/all_points', getAllPoints);
 router.put('/put', updateUserPoints);
 
 export default router;
